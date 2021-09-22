83793aba6a0ab92d3c0a4dce88419e6d0bcc14a2
Sawyer Click <src2188@columbia.edu>
2021-09-21 23:20:26 -0500

    add emoji ico

--- src/lib/Progress.svelte
+++ src/lib/Progress.svelte
@@ -19,7 +19,8 @@
 <!-- 
 <progress min="0" max={length} value={$progress} class="fixed left-0 top-0  w-full color-red-400" /> -->
 
-<div class=" fixed top-0 left-0 w-full h-3">
-	<div class="w-full h-full bg-gray-200 absolute" />
-	<div class="h-full bg-yellow-500 absolute" style="width:{pct}%" />
+<div class=" fixed top-0 left-0 w-full h-6">
+	<div class="w-full h-full bg-gray-200 absolute" aria-hidden />
+	<div class="h-full bg-yellow-500 absolute" style="width:{pct}%" aria-hidden />
+	<p class="z-50 absolute top-1 right-5 leading-none">card {$step} of {length}</p>
 </div>
